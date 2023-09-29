2d139b1c219f252f84238dfe3cbc1dcc976e0abf
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-09-28 21:45:02 -0300

    Create lambda.py

---
+++ usabilidade-desenvolvimento-web/28-09-2023/lambda.py
@@ -0,0 +1,33 @@
+"""
+Funcao Lambda
+Funcao anonima que possui apenas uma linha
+"""
+
+lista = [
+    {
+        "nome":"Vitor",
+        "sobrenome":"Leaes"
+    },
+    {
+        "nome":"Vitor",
+        "sobrenome":"Antoni"
+    },
+    {
+        "nome":"Natally",
+        "sobrenome":"Souza"
+    },
+    {
+        "nome":"Mari",
+        "sobrenome":"Silva"
+    },
+    {
+        "nome":"Regis",
+        "sobrenome":"Antoni"
+    }
+]
+
+for i in lista:
+    print(i)
+
+print(lista[1])
+# Output → {'nome': 'Natally', 'sobrenome': 'Souza'}
