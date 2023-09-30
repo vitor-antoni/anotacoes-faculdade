3133358f56ee401aa0a43028aba0f98b8f3be8c2
Vitor Antoni <v.silva.antoni@gmail.com>
2023-09-30 00:17:53 -0300

    Update main.py

--- usabilidade-desenvolvimento-web/29-09-2023/Herança/main.py
+++ usabilidade-desenvolvimento-web/29-09-2023/Herança/main.py
@@ -1,4 +1,5 @@
 from carro import Carro
+from moto import Moto
 
 carro = Carro("Gol",2002,"Volkswagen","Gasolina",False,4)
 
@@ -12,3 +13,6 @@ print(f"Estado atual do carro: {carro.esta_ligado}")
 
 ### Para exibir todos os atributos da variável carro
 print(carro.__dict__)
+
+moto = Moto("CG 160 Titan", 2023, "Honda", "Gasolina",False,2)
+print(moto.passageiros)
