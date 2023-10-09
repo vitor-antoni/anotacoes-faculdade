2c8159ea7e55698bebea08db7348139633e67e8e
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-10-09 16:56:00 -0300

    Create operadores-aritmeticos.py

---
+++ netacad-python/operadores-aritmeticos.py
@@ -0,0 +1,20 @@
+## Potenciacao
+print(2 * 3)
+# output: 6
+
+print(2 ** 3)
+# output: 8
+
+## Divisao inteira
+print(6 // 3)
+# output: 2
+
+print(6 // 4)
+# output: 1
+
+## Resto da divisao
+print(12 % 2)
+# output: 0
+
+print(12 % 5)
+# output: 2
