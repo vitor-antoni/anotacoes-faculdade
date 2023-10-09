ad1484f2949b1be97759f30ecf34b8f22b08d873
Vitor Antoni <92998253+vitor-antoni@users.noreply.github.com>
2023-10-09 16:21:36 -0300

    Create comandos-print.py

---
+++ netacad-python/comandos-print.py
@@ -0,0 +1,17 @@
+print("My name", end=" ")
+print("is Vitor Antoni")
+
+'''
+  # Output
+  My name is Vitor Antoni
+'''
+
+print("My", "name", "is", "vitor", sep="-")
+
+'''
+  # Output
+  My-name-is-vitor
+'''
+
+print("I'm learning Python")
+print("I'm \"Learning\" Python")
