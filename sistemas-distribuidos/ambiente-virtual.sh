4ca6f43f21f1fb4159cbc77f350f6dc547d135c4
vitor-antoni <v.silva.antoni@gmail.com>
2023-10-23 20:06:50 -0300

    atualizacao venv scripts

--- sistemas-distribuidos/ambiente-virtual.sh
+++ sistemas-distribuidos/ambiente-virtual.sh
@@ -1,6 +1,13 @@
 # Para iniciar um ambiente virtual em Python
+python -m pip install --upgrade pip
+python -m pip install django
+
 python -m venv venv
 .\venv\Scripts\activate
 
 # Para desativar o ambiente virtual
 deactivate
+
+# Iniciar um projeto
+django-admin startproject {project_name} {directory}
+manage.py runserver
